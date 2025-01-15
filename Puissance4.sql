afab447a760344eaee005d87dfb01fc52952597b
lenny-vergerolle <vergerolle.lenny@gmail.com>
2025-01-15 14:15:06 +0100

    maj

--- Puissance4.sql
+++ Puissance4.sql
@@ -1,8 +1,12 @@
-CREATE TABLE Equipe (
-    id INT PRIMARY KEY,
-    nom VARCHAR(50) UNIQUE NOT NULL,
-    symbole CHAR(1) NOT NULL,
-    score INT DEFAULT 0
+IF EXISTS DROP TABLE PARTIE;
+IF EXISTS DROP TABLE EQUIPE;
+
+
+CREATE TABLE EQUIPE (
+    idEqu INT PRIMARY KEY,
+    nomEqu VARCHAR(50) UNIQUE NOT NULL,
+    couleurEqu CHAR(1) NOT NULL,
+    scoreEqu INT DEFAULT 0
 );
 
 INSERT INTO Equipe (id, nom, symbole) VALUES
@@ -10,3 +14,9 @@ INSERT INTO Equipe (id, nom, symbole) VALUES
 (1, 'JAUNE', 'J'),
 (2, 'ROUGE', 'R');
 
+CREATE TABLE PARTIE (
+    idPart INT PRIMARY KEY,
+    datePart DATE NOT NULL,
+    idEquGagn INT,
+    FOREIGN KEY (idEquGagn) REFERENCES Equipe(idEqu)
+);
--- Puissance4Terminal/model/bd/EquipeBD.java
+++ Puissance4Terminal/model/bd/EquipeBD.java
@@ -1,5 +1,32 @@
 package Puissance4Terminal.model.bd;
 
+
+import java.sql.*;
+import java.util.ArrayList;
+import Puissance4Terminal.model.Equipe;
+import BD.ConnexionMySQL;
+
 public class EquipeBD {
+
+    ConnexionMySQL laConnexionMySQL;
+    Statement st;
+
+    public EquipeBD(ConnexionMySQL laConnexionMySQL) {
+        this.laConnexionMySQL = laConnexionMySQL;
+    }
+
+    public int getScore(Equipe equipe) throws SQLException {
+        String query = "SELECT (score) AS score FROM EQUIPE WHERE nom = '" + equipe.getNom() + "'";
+        Statement stm = this.laConnexionMySQL.createStatement();
+        ResultSet rs = stm.executeQuery(query);
+        int score = 0;
+
+        if (rs.next()) {
+            score = rs.getInt("score");
+        }
+        rs.close();
+        stm.close();
+        return score;
+    }
     
 }
